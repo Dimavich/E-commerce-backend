e8a44e0837aa63d7183d60fa31085422982b7c0f
Dmitriy <dmitriy.babich0@gmail.com>
2021-06-27 21:21:27 -0700

    all routs work

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -57,6 +57,7 @@ router.put('/:id', async(req, res) => {
     if(!tagData){
       res.status(404).json({message:`No Tag with that id exists`})
     }
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -73,6 +74,7 @@ router.delete('/:id', async(req, res) => {
     if (!tagData) {
       res.status(404).json({message:`No Tag with that id exists`})
     }
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
