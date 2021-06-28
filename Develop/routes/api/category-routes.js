3d1c494e70b38bdd7ba3750922a9db15310bb05f
Dmitriy <dmitriy.babich0@gmail.com>
2021-06-27 20:28:42 -0700

    all category routs work

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -55,6 +55,7 @@ router.put('/:id', async(req, res) => {
     if(!categoryData){
       res.status(404).json({message:`No Category with that id exists`})
     }
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -71,6 +72,7 @@ router.delete('/:id', async(req, res) => {
     if (!categoryData) {
       res.status(404).json({message:`No Category with that id exists`})
     }
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
